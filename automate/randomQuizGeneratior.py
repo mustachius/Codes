af7e9c04153f3fa3529d4b6841b0f9881fc62ac3
victorsartor <jvsaga@gmail.com>
2021-03-23 19:30:52 -0300

    adicionado código que gera os 35 arquivos de questionário e resposta

--- automate/randomQuizGeneratior.py
+++ automate/randomQuizGeneratior.py
@@ -10,3 +10,19 @@ capitais = {'Acre':'Rio Branco', 'Alagoas':'Maceio', 'Amapá':'Macapá', 'Amazon
             'Goiás':'Goiânia', 'Maranhão':'São Luís', 'Mato Grosso':'Cuiabá', 'Mato Grosso do Sul':'Campo Grande', 'Minas Gerais':'Belo Horizonte', 'Pará':'Belém',
             'Paraíba':'João Pessoa', 'Piauí':'Teresina', 'Rio de Janeiro':'Rio de Janeiro', 'Rio Grande do Sul':'Porto Alegre', 'Rondônia':'Porto Velho', 'Roraima':'Boa Vista'
             'Santa Catarina':'Florianópolis', 'São Paulo':'São Paulo', 'Sergipe':'Aracaju', 'Tocantins':'Palmas', 'Distrito Federal':'Brasília'}
+
+# Gerando 35 arquivos com as provas e os gabaritos das respostas.
+
+for quizNum in range(35):
+    # Criando os arquivos com os questionários e com os gabaritos de resposta
+    quizFile = open('capitaisquiz%s.txt' % (quizNum + 1), 'w')
+    answerKeyFile = open('capitaisquiz_respostas%s.txt' % (quizNum + 1), 'w')
+
+    # Escrevendo o cabeçalho da prova
+    quizFile.write('Nome:\n\nData:\n\nPeríodo:\n\n')
+    quizFile.write(('' * 20) + 'Questionários sobre as capitais brasileiras (Form %s)' %(quizNum + 1))
+    quizFile.write('\n\n')
+
+    # Embaralhando a ordem dos estados
+    stados = list(capitais.keys())
+    random.shuffle(stados)
