4e8745ee5acf98ff17d140f49e68f14ee6b7c505
victorsartor <jvsaga@gmail.com>
2021-03-23 19:04:09 -0300

    first commit

---
+++ automate/randomQuizGeneratior.py
@@ -0,0 +1,8 @@
+#! python3
+# randomQuizGenerator.py - Cria provas com perguntas e respostas em ordem aleatória,
+# juntamente com os gabaritos contendo as respostas.
+
+import random
+
+# Os dados para as provas estão dispostos no formato de dicionários. As chaves são os estados e os valores são as capitais.
+
